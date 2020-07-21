314d9acc6b248f62878017be534cb2a033c29cda
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-21 15:22:44 -0600

    add uniqueness of email for users table

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it {should validate_presence_of :email}
     it {should validate_presence_of :name}
+    it {should validates_uniqueness_of :email}
   end
 
 end
