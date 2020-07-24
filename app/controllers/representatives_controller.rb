96b8c707edf26532df27e30004b0138d68309c6d
Derek Borski <derek.borski@gmail.com>
2020-07-23 23:50:13 -0600

    Add @members, @senators, and @state instance variables to index action

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -1,4 +1,7 @@
 class RepresentativesController < ApplicationController
   def index
+    @members = HouseMember.by_state(params[:state_abbreviation])
+    @senators = Senator.by_state(params[:state_abbreviation])
+    @state = ::States[params[:state_abbreviation].to_sym]
   end 
 end
