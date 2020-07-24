9341c29fdf103eddab9a494405e6555c69ed6cc8
Derek Borski <derek.borski@gmail.com>
2020-07-23 23:49:20 -0600

    Create by_state class method spec

--- spec/models/senators_spec.rb
+++ spec/models/senators_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Senator, type: :model do
+  describe 'class methods' do
+    before(:each) do
+      @senator1 = create(:senator, state: "CO")
+      @senator2 = create(:senator, state: "ME")
+      @senator3 = create(:senator, state: "MN")
+      @senator4 = create(:senator, state: "CO")
+    end
+
+    it 'by_state' do
+
+      expect(Senator.by_state("CO")).to eq([@senator1, @senator4])
+    end
+  end
 end
