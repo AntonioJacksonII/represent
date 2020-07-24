d8d0a974997eae9b8eb24fcb8bcb3c863dc104ac
Derek Borski <derek.borski@gmail.com>
2020-07-23 22:28:38 -0600

    Begin representative index spec

---
+++ spec/features/representatives/representative_index_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+RSpec.describe "Representatives Index Page" do
+  describe "Issue-18 As a visitor, when I select a state from the dropdown menu" do
+    before(:each) do
+      @member1 = create(:house_member, state: "CO")
+      @member2 = create(:house_member, state: "CO")
+      @member3 = create(:house_member, state: "CO")
+      @member4 = create(:house_member, state: "CO")
+      @member5 = create(:house_member, state: "CO")
+      @member6 = create(:house_member, state: "CO")
+      @member7 = create(:house_member, state: "CO")
+      @member8 = create(:house_member, state: "CA")
+      @member9 = create(:house_member, state: "TX")
+      @member10 = create(:house_member, state: "AL")
+
+      @senator1 = create(:senator, state: "CO")
+      @senator2 = create(:senator, state: "CO")
+      @senator3 = create(:senator, state: "MN")
+      @senator4 = create(:senator, state: "CT")
+    end
+
+    it 'it displays the names of all house members from that state' do 
+      visit '/representatives/CO'
+
+      expect(page).to have_css('.member', count: 7)
+
+      within(first('.member')) do
+        within('.name') do
+          expect(page).to have_content("#{@member1.first_name} #{@member1.last_name}")
+        end
+      end
+    end 
+
+    it 'it displays the names of all senate members from that state' do 
+      visit '/representatives/CO'
+
+      expect(page).to have_css('.senator', count: 2)
+
+      within(first('.senator')) do
+        within('.name') do
+          expect(page).to have_content("#{@senator1.first_name} #{@senator1.last_name}")
+        end
+      end
+
+    end 
+  end
+end
+
+
+# As a visitor or user, when I visit the representatives index page 
+
+# and select a state from the dropdown menu
+
+# All of the house members and senators of the selected state are displayed 
+
+#in two separate columns on the page
