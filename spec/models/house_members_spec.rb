2d5899327df37ac63336289c8f08156872a0666e
Derek Borski <derek.borski@gmail.com>
2020-07-23 23:41:09 -0600

    Add by_state class method spec

--- spec/models/house_members_spec.rb
+++ spec/models/house_members_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe HouseMember, type: :model do
+  describe 'class methods' do
+    before(:each) do
+      @member1 = create(:house_member, state: "CO")
+      @member2 = create(:house_member, state: "AL")
+      @member3 = create(:house_member, state: "CO")
+      @member4 = create(:house_member, state: "TX")
+    end
+
+    it 'by_state' do
+
+      expect(HouseMember.by_state("CO")).to eq([@member1, @member3])
+    end
+  end
 end
