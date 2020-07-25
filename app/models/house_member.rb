e0d108c5df3993fd10e08537c8623420be3373bf
Derek Borski <derek.borski@gmail.com>
2020-07-24 20:34:05 -0600

    Add full_name instance method and spec

--- app/models/house_member.rb
+++ app/models/house_member.rb
@@ -3,4 +3,8 @@ class HouseMember < ApplicationRecord
   def self.by_state(state_abbrev)
     where(state: "#{state_abbrev}")
   end 
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end 
 end
