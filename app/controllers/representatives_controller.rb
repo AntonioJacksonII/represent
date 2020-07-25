bd70ec62aa7040af1df0a30d278af45d20304048
Derek Borski <derek.borski@gmail.com>
2020-07-24 23:59:42 -0600

    Now no longer need to convert state params to symbol

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -2,6 +2,6 @@ class RepresentativesController < ApplicationController
   def index
     @members = HouseMember.by_state(params[:state_abbreviation])
     @senators = Senator.by_state(params[:state_abbreviation])
-    @state = ::States[params[:state_abbreviation].to_sym]
+    @state = ::States[params[:state_abbreviation]]
   end 
 end
