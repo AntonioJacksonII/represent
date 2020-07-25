3b9df0f938548100bbd3a134c5239647df48f699
Derek Borski <derek.borski@gmail.com>
2020-07-24 23:14:23 -0600

    Add manual congress_id to all factories so that images display for each rep

--- spec/features/representatives/representative_index_spec.rb
+++ spec/features/representatives/representative_index_spec.rb
@@ -3,25 +3,27 @@ require 'rails_helper'
 RSpec.describe "Representatives Index Page" do
   describe "Issue-18 As a visitor, when I select a state from the dropdown menu" do
     before(:each) do
-      @member1 = create(:house_member, state: "CO")
-      @member2 = create(:house_member, state: "FL")
-      @member3 = create(:house_member, state: "CO")
-      @member4 = create(:house_member, state: "CO")
-      @member5 = create(:house_member, state: "CA")
-      @member6 = create(:house_member, state: "CO")
-      @member7 = create(:house_member, state: "CO")
-      @member8 = create(:house_member, state: "CA")
-      @member9 = create(:house_member, state: "CO")
-      @member10 = create(:house_member, state: "CO")
+      @member1 = create(:house_member, state: "CO", congress_id: "A000374")
+      @member2 = create(:house_member, state: "FL", congress_id: "E000297")
+      @member3 = create(:house_member, state: "CO", congress_id: "E000296")
+      @member4 = create(:house_member, state: "CO", congress_id: "E000296")
+      @member5 = create(:house_member, state: "CA", congress_id: "E000296")
+      @member6 = create(:house_member, state: "CO", congress_id: "E000296")
+      @member7 = create(:house_member, state: "CO", congress_id: "E000296")
+      @member8 = create(:house_member, state: "CA", congress_id: "E000296")
+      @member9 = create(:house_member, state: "CO", congress_id: "E000296")
+      @member10 = create(:house_member, state: "CO", congress_id: "E000296")
 
-      @senator1 = create(:senator, state: "CO")
-      @senator2 = create(:senator, state: "CO")
-      @senator3 = create(:senator, state: "MN")
-      @senator4 = create(:senator, state: "CT")
+      @senator1 = create(:senator, state: "CO", congress_id: "E000296")
+      @senator2 = create(:senator, state: "CO", congress_id: "E000296")
+      @senator3 = create(:senator, state: "MN", congress_id: "E000296")
+      @senator4 = create(:senator, state: "CT", congress_id: "E000296")
     end
 
     it 'it displays the name of the state in the header' do 
       visit '/representatives/CO'
+
+      binding.pry
 
       expect(page).to have_content("Colorado")
 
