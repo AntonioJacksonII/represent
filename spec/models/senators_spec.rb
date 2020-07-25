86d9a37d3e6c4b26a883c6d96716268552e6b998
Derek Borski <derek.borski@gmail.com>
2020-07-24 20:34:20 -0600

    Add full_name instance method and spec

--- spec/models/senators_spec.rb
+++ spec/models/senators_spec.rb
@@ -14,4 +14,18 @@ RSpec.describe Senator, type: :model do
       expect(Senator.by_state("CO")).to eq([@senator1, @senator4])
     end
   end
+
+  describe 'instance methods' do
+    before(:each) do
+      @senator1 = create(:senator, state: "CO")
+      @senator2 = create(:senator, state: "ME")
+      @senator3 = create(:senator, state: "MN")
+      @senator4 = create(:senator, state: "CO")
+    end
+
+    it 'full_name' do
+
+      expect(@senator1.full_name).to eq("#{@senator1.first_name} #{@senator1.last_name}")
+    end
+  end
 end
