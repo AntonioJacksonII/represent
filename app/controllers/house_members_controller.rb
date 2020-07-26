1737fdb3698dc12cba3ff253a7f4d5c014dd3fce
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-26 16:26:07 -0600

    Added other reps and senators to display on the side nav bar

--- app/controllers/house_members_controller.rb
+++ app/controllers/house_members_controller.rb
@@ -3,6 +3,8 @@ class HouseMembersController < ApplicationController
   def show
     id = house_member_params[:id]
     @rep = HouseMember.find(id)
+    @other_reps = HouseMember.by_state(@rep.state)
+    @senators = Senator.by_state(@rep.state)
   end
 
   private
