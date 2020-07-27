43227a50fcea969cf5b5f76bac55676bedba2b59
Derek Borski <derek.borski@gmail.com>
2020-07-26 19:26:37 -0600

    Add pseudocode

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,3 +1,11 @@
 class DashboardController < ApplicationController
-  def show;end
+  def show
+    # @house_favorites = current_user.house_favorites
+    # @senator_favorites = current_user.senator_favorites
+    # @news_articles = ArticleSearch.new.get_articles(all_favorites(@house_favorites, @senator_favorites), "en", "popularity", 5)
+  end
+
+  # def all_favorites(members, senators)
+  #   # Need to combine members names and senators names into one array
+  # end 
 end
