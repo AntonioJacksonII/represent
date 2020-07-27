94cb13744ad22333a24440fe9d3019e05fbaa474
Derek Borski <derek.borski@gmail.com>
2020-07-27 09:25:00 -0600

    Create article_search

---
+++ app/models/article_search.rb
@@ -0,0 +1,11 @@
+class ArticleSearch
+  def get_articles(favorite_names, sort, language_abbrev, num_results)
+    service.get_articles(favorite_names, sort, language_abbrev, num_results)[:data].map do |data|
+      Article.new(data)
+    end
+  end 
+
+  def service
+    RequestService.new
+  end
+end
