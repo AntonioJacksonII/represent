62d6a05e7604696e69fbfe5c9e5ea5ee2e786167
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-26 23:09:49 -0600

    add javascript for dropdown on bills index page and css for hoverable feature in bills index page

--- app/assets/javascripts/selections/dropdown.js
+++ app/assets/javascripts/selections/dropdown.js
@@ -7,3 +7,12 @@ function selectFromSearch(){
   var selectedSearch = document.getElementById("search_by_name").value;
   window.location = "/representatives/" + selectedSearch
 }
+
+$(function(){
+
+  $(".dropdown-menu").on('click', 'li', function(){
+    $(".topic:first-child").text($(this).text());
+    $(".topic:first-child").val($(this).text());
+ });
+
+});
