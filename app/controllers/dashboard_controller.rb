605c5449495a21a7695ff13835354ac831bbf56b
Derek Borski <derek.borski@gmail.com>
2020-07-27 00:19:10 -0600

    Update show action and add all_favorites private method

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,11 +1,15 @@
 class DashboardController < ApplicationController
   def show
-    # @house_favorites = current_user.house_favorites
-    # @senator_favorites = current_user.senator_favorites
-    # @news_articles = ArticleSearch.new.get_articles(all_favorites(@house_favorites, @senator_favorites), "en", "popularity", 5)
+    @house_favorites = current_user.house_members
+    @senator_favorites = current_user.senators
+    @news_articles = ArticleSearch.new.get_articles(all_favorites(@house_favorites, @senator_favorites), "popularity", "en", 5)
   end
-
-  # def all_favorites(members, senators)
-  #   # Need to combine members names and senators names into one array
-  # end 
+  
+  
+  private
+  
+  def all_favorites(members, senators)
+    all_favs = members.to_a.concat(senators.to_a)
+    all_favs.map(&:full_name).join(",")
+  end 
 end
