1d27c80d8f00e2420a0b118bf739d4e3253bd1a7
Antonio Jackson <antoniojackson@gmail.com>
2020-07-28 17:45:59 -0600

    Updating spec to search for the bill title link and to pass the query param through in the url

--- spec/features/votes/comparison_spec.rb
+++ spec/features/votes/comparison_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'on the comparison page' do
-  it 'lists the bill(s) selected by the visitor/user' do
+  it 'lists a bill selected by the visitor/user' do
     json_response = File.read('spec/fixtures/bill.json')
     parser = PropublicaParser.new
     bill = parser.parse_bill(json_response, 0)
 
-    visit comparisons_path
+    visit "/comparisons?bill=#{bill.bill_id}"
 
     expect(page).to have_content('Compare Your Votes')
     expect(page).to have_content('For the bills below, select "YES" if you would vote for the bill or "NO" if you would vote against the bill.')
@@ -14,10 +14,13 @@ RSpec.describe 'on the comparison page' do
     expect(page).to have_css('#bills-table')
     within('#bills-table') do
       expect(page).to have_css('#bill-title')
+      expect(page).to have_link(bill.short_title)
       expect(page).to have_css('#bill-summary')
       expect(page).to have_css('#vote-yes')
       expect(page).to have_css('#vote-no')
     end
+
+    choose "#{bill.bill_id}_yes"
     click_button('Submit Your Votes and Get Your Comparison Score')
     expect(current_path).to eq(results_path)
   end
