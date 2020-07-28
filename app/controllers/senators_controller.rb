ef0c054026e07a4906e060ac2ef3fee045bbc379
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-28 14:27:44 -0600

    added state to present in view page

--- app/controllers/senators_controller.rb
+++ app/controllers/senators_controller.rb
@@ -4,6 +4,7 @@ class SenatorsController < ApplicationController
     @senator = Senator.find(id)
     @other_senators = Senator.by_state(@senator.state)
     @representatives = HouseMember.by_state(@senator.state)
+    @state = ::States[@senator.state]
   end
 
   private
