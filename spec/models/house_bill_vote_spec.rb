1aeee9664e6f088e34a8e06b43139277169370a4
Alex Pariseau <arpariseau@gmail.com>
2020-07-29 16:12:30 -0600

    Can now load bills from the service output fixture. Need to connect to service itself

---
+++ spec/models/house_bill_vote_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe HouseBillVote, type: :model do
+  describe 'validations' do
+    it {should validate_presence_of :roll_call}
+    it {should validate_presence_of :offset}
+  end
+
+  describe 'relationships' do
+    it {should belong_to :bill}
+  end
+end
