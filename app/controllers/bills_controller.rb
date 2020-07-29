d0e58c46b7ec32dfd5d61cc9b12acefd32f38ade
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-28 19:43:22 -0600

    passed through congress_id and full_name

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,5 +1,8 @@
 class BillsController < ApplicationController
+  layout "bills_layout"
   def index
     @subjects = Bill.topics
+    @name = params[:name]
+    @congress_id = params[:congress_id]
   end
 end
