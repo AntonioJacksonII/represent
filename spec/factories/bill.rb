0f95b15dae06427d5ceced7ddb8e1f76401dac0a
Derek Borski <derek.borski@gmail.com>
2020-07-29 10:33:32 -0600

    Create bill factory

---
+++ spec/factories/bill.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :bill do
+    summary_short { Faker::TvShows::DumbAndDumber.quote }
+    sequence(:congress_url) { |n| "https://bill#{n}.house.gov" }
+    short_title { Faker::GreekPhilosophers.quote }
+    primary_subject { Faker::Lorem.word }
+    offset { Faker::Number.number(digits: 2) }
+    passage_roll_call { Faker::Number.number(digits: 3) }
+  end
+end
