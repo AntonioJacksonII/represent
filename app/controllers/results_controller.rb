45c6ca08b08495c430a045d38fa5c875b788ab12
Derek Borski <derek.borski@gmail.com>
2020-07-29 17:03:56 -0600

    Begin create action methods and update spec Co-authored-by: AntonioJacksonII <antoniojackson@gmail.com>

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -4,5 +4,36 @@ class ResultsController < ApplicationController
   end
 
   def create
+    yes_array = []
+    no_array = []
+    
+    results_params.map do |key, value|
+      if value == "yes"
+        yes_array << key
+      elsif value == "no"
+        no_array << key
+      end 
+    end 
+
+    if Senator.includes?(params[:congress_id])
+      #lookup senate bill vote
+    elsif 
+      HouseMember.includes?(params[:congress_id])
+      yes_roll_calls = []
+      yes_array.each do |bill_id|
+        yes_roll_calls << Bill.find_by(bill_id: bill_id).house_bill_vote.roll_call
+      end 
+      binding.pry
+      #lookup house bill vote
+      Bill.find_by(bill_id: yes_array)
+    else
+      #flash message indicating that id is not a housemember or senator
+    end 
+  end 
+
+  private
+
+  def results_params
+    params.permit!.to_h
   end 
 end
--- spec/features/results/results_spec.rb
+++ spec/features/results/results_spec.rb
@@ -7,31 +7,36 @@ describe "Logged in User" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
 
     @aoc = HouseMember.create!(first_name: 'Alexandria', last_name: 'Ocasio-Cortez', congress_id: "O000172")
-    json_response = File.read('spec/fixtures/bill.json')
-    parser = PropublicaParser.new
-    @bill_1 = parser.parse_bill(json_response, 0)
-    @bill_2 =   Bill.create!(id: 20,
-        bill_id: "hr1957-116",
-        summary_short:
-         "Great American Outdoors Act This bill establishes the National Parks and Public Land Legacy Restoration Fund to support deferred maintenance projects on federal lands.  For FY2021-FY2025, there shall be deposited into the fund an amount equal to 50% of energy development revenues credited, covered, or deposited as miscellaneous receipts from oil, gas, coal, or alternative or renewable energy development on federal lands and waters. Deposited amounts must not exceed $1.9 billion for any fiscal...",
-        congress_url: "https://www.congress.gov/bill/116th-congress/house-bill/1957",
-        short_title: "Great American Outdoors Act",
-        primary_subject: "Armed Forces and National Security",
-        offset: 0,
-        created_at: "Tue, 28 Jul 2020 19:37:05 UTC +00:00",
-        updated_at: "Tue, 28 Jul 2020 19:37:05 UTC +00:00",
-        passage_roll_call: 153)
+    # json_response = File.read('spec/fixtures/bill.json')
+    # parser = PropublicaParser.new
+    # @bill_1 = parser.parse_bill(json_response, 0)
+    @bill_1 =   Bill.create!(
+      bill_id: "hr6395-116",
+      summary_short:
+      "To authorize appropriations for fiscal year 2021 for military activities of the Department of Defense and for military construction, to prescribe military personnel strengths for such fiscal year, and for other purposes.",
+      congress_url: "https://www.congress.gov/bill/116th-congress/house-bill/6395",
+      short_title: "William M. (Mac) Thornberry National Defense Authorization Act for Fiscal Year 2021",
+      primary_subject: "Armed Forces and National Security"
+    )
+
+    @bill_2 =   Bill.create!(
+      bill_id: "hr1957-116",
+      summary_short:
+      "Great American Outdoors Act This bill establishes the National Parks and Public Land Legacy Restoration Fund to support deferred maintenance projects on federal lands.  For FY2021-FY2025, there shall be deposited into the fund an amount equal to 50% of energy development revenues credited, covered, or deposited as miscellaneous receipts from oil, gas, coal, or alternative or renewable energy development on federal lands and waters. Deposited amounts must not exceed $1.9 billion for any fiscal...",
+      congress_url: "https://www.congress.gov/bill/116th-congress/house-bill/1957",
+      short_title: "Great American Outdoors Act",
+      primary_subject: "Armed Forces and National Security",
+      created_at: "Tue, 28 Jul 2020 19:37:05 UTC +00:00",
+      updated_at: "Tue, 28 Jul 2020 19:37:05 UTC +00:00")
+    
+    @house_bill_vote1 = create(:house_bill_vote, roll_call: 153, offset: 0, bill_id: @bill_1.id)
+    @house_bill_vote2 = create(:house_bill_vote, roll_call: 152, offset: 20, bill_id: @bill_2.id)
 
     visit "/comparison?topic=Armed%20Forces%20and%20National%20Security&id=#{@aoc.congress_id}"
     choose "#{@bill_1.bill_id}_yes"
     choose "#{@bill_2.bill_id}_no"
     click_button('Submit Your Votes and Get Your Comparison Score')
-
-    @house_bill_vote1 = create(:house_bill_vote, house_roll_call: 153, house_offset: 0, bill_id: @bill_1.id)
-    @house_bill_vote2 = create(:house_bill_vote, house_roll_call: 152, house_offset: 20, bill_id: @bill_2.id)
-
-
-
+    
 
     # We are passed in from the comparisions page a congress_id, each bill's ID and whether the user voted yes or no
     # Need to identify whether the congress_id is a house or senate member
@@ -51,7 +56,7 @@ describe "Logged in User" do
 
   it "User sees results from comparison with politician" do
 
-    binding.pry
+    # binding.pry
 
     # visit dashboard_path
 
