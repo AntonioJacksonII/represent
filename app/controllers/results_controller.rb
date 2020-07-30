298c68cdb025158296f052d314127766b33e36d3
Derek Borski <derek.borski@gmail.com>
2020-07-29 21:52:57 -0600

    Add senator results spec and update create action with senator conditional
    Co-authored-by: AntonioJacksonII <antoniojackson@gmail.com>

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,6 +1,7 @@
 class ResultsController < ApplicationController
 
   def show
+    binding.pry
   end
 
   def create
@@ -16,16 +17,106 @@ class ResultsController < ApplicationController
     end 
 
     if Senator.includes?(params[:congress_id])
-      #lookup senate bill vote
-    elsif 
-      HouseMember.includes?(params[:congress_id])
-      yes_roll_calls = []
+      yes_roll_calls = {}
       yes_array.each do |bill_id|
-        yes_roll_calls << Bill.find_by(bill_id: bill_id).house_bill_vote.roll_call
+        yes_roll_calls[Bill.find_by(bill_id: bill_id).senate_bill_vote.roll_call] = Bill.find_by(bill_id: bill_id).senate_bill_vote.offset
       end 
-      binding.pry
-      #lookup house bill vote
-      Bill.find_by(bill_id: yes_array)
+
+      no_roll_calls = {}
+      no_array.each do |bill_id|
+        no_roll_calls[Bill.find_by(bill_id: bill_id).senate_bill_vote.roll_call] = Bill.find_by(bill_id: bill_id).senate_bill_vote.offset
+      end
+      
+      yes_hash = {}
+      yes_roll_calls.each do |roll_call, offset|
+        yes_hash["#{roll_call}"] = SinatraService.new.get_member_votes(results_params[:congress_id], offset)["data"]["#{roll_call}"]
+      end 
+
+      no_hash = {}
+      no_roll_calls.each do |roll_call, offset|
+        no_hash["#{roll_call}"] = SinatraService.new.get_member_votes(results_params[:congress_id], offset)["data"]["#{roll_call}"]
+      end 
+      
+      matching_array = []
+      not_matching_array = []
+      yes_hash.each do |roll_call, member_vote|
+        if member_vote == "Yes"
+          matching_array << roll_call
+        elsif member_vote == "No"
+          not_matching_array << roll_call
+        end 
+      end 
+
+      no_hash.each do |roll_call, member_vote|
+        if member_vote == "Yes"
+          not_matching_array << roll_call
+        elsif member_vote == "No"
+          matching_array << roll_call
+        end 
+      end 
+
+      not_matching_bills = not_matching_array.map do |roll_call_num|
+        SenateBillVote.find_by(roll_call: "#{roll_call_num}").bill.short_title
+      end 
+
+      matching_bills = matching_array.map do |roll_call_num|
+        SenateBillVote.find_by(roll_call: "#{roll_call_num}").bill.short_title
+      end 
+
+      comparison_score = matching_array.length.to_f / (matching_array.length + not_matching_array.length).to_f
+      
+      redirect_to results_path(params[:congress_id], comparison_score: comparison_score, matching_bills: matching_bills, not_matching_bills: not_matching_bills)
+
+    elsif HouseMember.includes?(params[:congress_id])
+      yes_roll_calls = {}
+      yes_array.each do |bill_id|
+        yes_roll_calls[Bill.find_by(bill_id: bill_id).house_bill_vote.roll_call] = Bill.find_by(bill_id: bill_id).house_bill_vote.offset
+      end 
+
+      no_roll_calls = {}
+      no_array.each do |bill_id|
+        no_roll_calls[Bill.find_by(bill_id: bill_id).house_bill_vote.roll_call] = Bill.find_by(bill_id: bill_id).house_bill_vote.offset
+      end
+      
+      yes_hash = {}
+      yes_roll_calls.each do |roll_call, offset|
+        yes_hash["#{roll_call}"] = SinatraService.new.get_member_votes(results_params[:congress_id], offset)["data"]["#{roll_call}"]
+      end 
+
+      no_hash = {}
+      no_roll_calls.each do |roll_call, offset|
+        no_hash["#{roll_call}"] = SinatraService.new.get_member_votes(results_params[:congress_id], offset)["data"]["#{roll_call}"]
+      end 
+      
+      matching_array = []
+      not_matching_array = []
+      yes_hash.each do |roll_call, member_vote|
+        if member_vote == "Yes"
+          matching_array << roll_call
+        elsif member_vote == "No"
+          not_matching_array << roll_call
+        end 
+      end 
+
+      no_hash.each do |roll_call, member_vote|
+        if member_vote == "Yes"
+          not_matching_array << roll_call
+        elsif member_vote == "No"
+          matching_array << roll_call
+        end 
+      end 
+
+      not_matching_bills = not_matching_array.map do |roll_call_num|
+        HouseBillVote.find_by(roll_call: "#{roll_call_num}").bill.short_title
+      end 
+
+      matching_bills = matching_array.map do |roll_call_num|
+        HouseBillVote.find_by(roll_call: "#{roll_call_num}").bill.short_title
+      end 
+
+      comparison_score = matching_array.length.to_f / (matching_array.length + not_matching_array.length).to_f
+      
+      redirect_to results_path(params[:congress_id], comparison_score: comparison_score, matching_bills: matching_bills, not_matching_bills: not_matching_bills)
     else
       #flash message indicating that id is not a housemember or senator
     end 
--- spec/features/results/results_spec.rb
+++ spec/features/results/results_spec.rb
@@ -7,9 +7,8 @@ describe "Logged in User" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
 
     @aoc = HouseMember.create!(first_name: 'Alexandria', last_name: 'Ocasio-Cortez', congress_id: "O000172")
-    # json_response = File.read('spec/fixtures/bill.json')
-    # parser = PropublicaParser.new
-    # @bill_1 = parser.parse_bill(json_response, 0)
+    @lamar = Senator.create!(first_name: 'Lamar', last_name: 'Alexander', congress_id: "A000360")
+
     @bill_1 =   Bill.create!(
       bill_id: "hr6395-116",
       summary_short:
@@ -19,6 +18,7 @@ describe "Logged in User" do
       primary_subject: "Armed Forces and National Security"
     )
 
+    
     @bill_2 =   Bill.create!(
       bill_id: "hr1957-116",
       summary_short:
@@ -28,45 +28,56 @@ describe "Logged in User" do
       primary_subject: "Armed Forces and National Security",
       created_at: "Tue, 28 Jul 2020 19:37:05 UTC +00:00",
       updated_at: "Tue, 28 Jul 2020 19:37:05 UTC +00:00")
-    
+      
+    @bill_3 =   Bill.create!(
+      bill_id: "s4049-116",
+      summary_short:
+      "To authorize appropriations for fiscal year 2021 for military activities of the Department of Defense and for military construction, to prescribe military personnel strengths for such fiscal year, and for other purposes.",
+      congress_url: "https://www.congress.gov/bill/116th-congress/house-bill/6395",
+      short_title: "William M. (Mac) Thornberry National Defense Authorization Act for Fiscal Year 2021",
+      primary_subject: "Armed Forces and National Security"
+     )
+
+    # @bill_4 =   Bill.create!(
+    #   bill_id: "hr1957-116",
+    #   summary_short:
+    #   "To authorize appropriations for fiscal year 2021 for military activities of the Department of Defense and for military construction, to prescribe military personnel strengths for such fiscal year, and for other purposes.",
+    #   congress_url: "https://www.congress.gov/bill/116th-congress/house-bill/6395",
+    #   short_title: "William M. (Mac) Thornberry National Defense Authorization Act for Fiscal Year 2021",
+    #   primary_subject: "Armed Forces and National Security"
+    #  )
+
     @house_bill_vote1 = create(:house_bill_vote, roll_call: 153, offset: 0, bill_id: @bill_1.id)
     @house_bill_vote2 = create(:house_bill_vote, roll_call: 152, offset: 20, bill_id: @bill_2.id)
 
-    visit "/comparison?topic=Armed%20Forces%20and%20National%20Security&id=#{@aoc.congress_id}"
-    choose "#{@bill_1.bill_id}_yes"
-    choose "#{@bill_2.bill_id}_no"
-    click_button('Submit Your Votes and Get Your Comparison Score')
-    
+    @senate_bill_vote1 = SenateBillVote.create!(roll_call: 140, offset: 0, bill: @bill_3)
+    @senate_bill_vote1 = SenateBillVote.create!(roll_call: 121, offset: 20, bill: @bill_2)
 
+    
+    
     # We are passed in from the comparisions page a congress_id, each bill's ID and whether the user voted yes or no
     # Need to identify whether the congress_id is a house or senate member
     # Then we need to look up in the BillVotes table and get role call # and the offset
     # Then we hit our member votes api and see how the member voted on each bill 
     # One we have that information, compare that with how the user voted, then create a comparison score
     # and then save the comparison score on the respective favorites table
-
-    # <ActionController::Parameters {"utf8"=>"✓", 
-    #                                "authenticity_token"=>"5Gf72jXaZfaYZbuGTdQr9GYUYoH6+yt62i4ih2RLWch48REkHi3cq8Dc5F5Ts91coqtOLZ16FxvDISWLv9P5qw==", 
-    #                                "hr6395-116"=>"yes", "hr1957-116"=>"no", "commit"=>"Submit Your Votes and Get Your Comparison Score", 
-    #                                "controller"=>"results", 
-    #                                "action"=>"show", 
-    #                                "congress_id"=>"O000172"} permitted: false>
     
   end
+  
+  it "User sees results from comparison with house member", :vcr do
+    
+    visit "/comparison?topic=Armed%20Forces%20and%20National%20Security&id=#{@aoc.congress_id}"
+    choose "#{@bill_1.bill_id}_yes"
+    choose "#{@bill_2.bill_id}_yes"
+    click_button('Submit Your Votes and Get Your Comparison Score')
+  end
 
-  it "User sees results from comparison with politician" do
+  it "User sees results from comparison with senator", :vcr do
 
-    # binding.pry
-
-    # visit dashboard_path
-
-    # expect(page).to have_css(".cards", count: 6)
-
-    # within(first('.cards')) do
-    #   expect(page).to have_css(".card-title")
-    #   expect(page).to have_css(".card-source")
-    #   expect(page).to have_css(".card-link")
-    # end
+    visit "/comparison?topic=Armed%20Forces%20and%20National%20Security&id=#{@lamar.congress_id}"
+    choose "#{@bill_3.bill_id}_yes"
+    choose "#{@bill_2.bill_id}_no"
+    click_button('Submit Your Votes and Get Your Comparison Score')
   end
 
   xit "User has a prefabbed twitter response to send to representative" do
