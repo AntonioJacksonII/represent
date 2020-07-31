f427a19d1a0bc9022044df3723fb2e6a354bde30
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-07-30 19:08:52 -0600

    update logo and styling on pages

--- app/controllers/comparison_controller.rb
+++ app/controllers/comparison_controller.rb
@@ -4,9 +4,15 @@ class ComparisonController < ApplicationController
     bill_id = params[:bill]
     selected_subject = params[:topic]
     congress_id = params[:id]
-    @bill = Bill.where(bill_id: bill_id).first
     @bills = Bill.where(primary_subject: selected_subject)
     @rep = HouseMember.where(congress_id: congress_id).first || Senator.where(congress_id: congress_id).first
+    if bill_id
+      @bill = Bill.where(bill_id: bill_id).first
+      if @bill.nil?
+        flash[:alert] = "This bill does not exist"
+        redirect_to "/bills?name=#{@rep.full_name}&congress_id=#{congress_id}"
+      end
+    end
   end
 
   private
