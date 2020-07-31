a80a2fc16c4a181d8032a140a3ce44231d46418e
Derek Borski <derek.borski@gmail.com>
2020-07-30 18:45:00 -0600

    Account for bills

--- app/controllers/comparison_controller.rb
+++ app/controllers/comparison_controller.rb
@@ -4,19 +4,29 @@ class ComparisonController < ApplicationController
     bill_id = params[:bill]
     selected_subject = params[:topic]
     congress_id = params[:id]
-    @bill = Bill.where(bill_id: bill_id).first
+    @rep = HouseMember.where(congress_id: congress_id).first || Senator.where(congress_id: congress_id).first
+    bill = Bill.where(bill_id: bill_id).first
     all_bills = Bill.where(primary_subject: selected_subject)
     
-    @rep = HouseMember.where(congress_id: congress_id).first || Senator.where(congress_id: congress_id).first
-
-    if @rep.class == HouseMember
-      @bills = all_bills.find_all do |bill|
-        bill.house_bill_vote
-      end
-    else 
-      @bills = all_bills.find_all do |bill|
-        bill.senate_bill_vote
-      end
+    if bill
+      if @rep.class == HouseMember
+        @bill = bill if bill.house_bill_vote
+      elsif @rep.class == Senator 
+        @bill = bill if bill.senate_bill_vote
+      else 
+        flash[:failure] = "This bill was not voted on by #{@rep.full_name}"
+        redirect_to bills_path
+      end 
+    elsif all_bills
+      if @rep.class == HouseMember
+        @bills = all_bills.find_all do |bill|
+          bill.house_bill_vote
+        end
+      else 
+        @bills = all_bills.find_all do |bill|
+          bill.senate_bill_vote
+        end
+      end 
     end 
   end
 
