043845233d6f5410754cb0f7b6be6d8502b05bc2
Antonio Jackson <antoniojackson@gmail.com>
2020-07-31 03:31:18 -0600

    Refactoring the comparison controller to be fewer lines without nested if statements

--- app/controllers/comparison_controller.rb
+++ app/controllers/comparison_controller.rb
@@ -15,24 +15,20 @@ class ComparisonController < ApplicationController
     bill = Bill.where(bill_id: bill_id).first
     all_bills = Bill.where(primary_subject: selected_subject)
 
-    if bill
-      if @rep.class == HouseMember && bill.house_bill_vote
-        @bill = bill
-      elsif @rep.class == Senator && bill.senate_bill_vote
-        @bill = bill 
-      else
-        flash[:failure] = "This bill was not voted on by #{@rep.full_name}"
-        redirect_to bills_path
+    if bill && @rep.class == HouseMember && bill.house_bill_vote
+      @bill = bill
+    elsif bill && @rep.class == Senator && bill.senate_bill_vote
+      @bill = bill
+    elsif bill
+      flash[:failure] = "This bill was not voted on by #{@rep.full_name}"
+      redirect_to bills_path
+    elsif all_bills && @rep.class == HouseMember
+      @bills = all_bills.find_all do |bill|
+        bill.house_bill_vote
       end
     elsif all_bills
-      if @rep.class == HouseMember
-        @bills = all_bills.find_all do |bill|
-          bill.house_bill_vote
-        end
-      else
-        @bills = all_bills.find_all do |bill|
-          bill.senate_bill_vote
-        end
+      @bills = all_bills.find_all do |bill|
+        bill.senate_bill_vote
       end
     end
   end
