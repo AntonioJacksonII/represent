7b16d1d1fb43a14de420f2f90cdf45ef37250a1c
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-08-10 15:57:31 -0600

    Added active record class method to pull five recent bills voted on

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -6,4 +6,12 @@ class Bill < ApplicationRecord
   def self.topics
     pluck(:primary_subject).uniq
   end
+
+  def self.find_recent_five(representative)
+    if representative.class == HouseMember
+      Bill.joins(:house_bill_vote).where('session = ?', 2).order('house_bill_votes.roll_call DESC').limit(5)
+    else
+      Bill.joins(:senate_bill_vote).where('session = ?', 2).order('senate_bill_votes.roll_call DESC').limit(5)
+    end
+  end
 end
