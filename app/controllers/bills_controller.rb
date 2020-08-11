5c9c2516836f870e9f34ea14c6ed5a196acf4145
Antonio Jackson <antoniojackson@gmail.com>
2020-08-11 11:51:46 -0600

    Updating BillsController to display subjects on the bill select page based on the selected representative's chamber

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,9 +1,10 @@
 class BillsController < ApplicationController
   layout "bills_layout"
   def index
-    @subjects = Bill.topics
     @name = params[:name]
     @congress_id = params[:congress_id]
     @representative = HouseMember.find_by(congress_id: @congress_id) || Senator.find_by(congress_id: @congress_id)
+    @subjects = Bill.house_topics if @representative.class == HouseMember
+    @subjects = Bill.senate_topics if @representative.class == Senator
   end
 end
