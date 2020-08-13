0b50c1fb05b0858a67809826fa7acbbd21dab6a2
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-08-12 18:39:10 -0600

    created pie charts for data using chart.js and added the cdn for library

---
+++ app/controllers/senator_vote_info_controller.rb
@@ -0,0 +1,4 @@
+class SenatorVoteInfoController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/house_members/vote_info', to: 'house_vote_info#index'
+  get '/senators/vote_info', to: 'senator_vote_info#index'
   resources :house_members, only: [:show]
   resources :senators, only: [:show]
   root to: 'home#index'
@@ -17,5 +19,3 @@ Rails.application.routes.draw do
   post '/results/:congress_id', to: 'results#create'
   get '/results/:congress_id', to: 'results#show', as: 'results'
 end
-
-
