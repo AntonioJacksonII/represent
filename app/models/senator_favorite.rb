9c348d5cc1f2d06bab8826db96a75e9e6f08e35e
Antonio Jackson <antoniojackson@gmail.com>
2020-08-13 18:32:02 -0600

    Correcting formulas in the update_aggregate_comparison methods

--- app/models/senator_favorite.rb
+++ app/models/senator_favorite.rb
@@ -4,9 +4,12 @@ class SenatorFavorite < ApplicationRecord
 
   def update_aggregate_comparison(results)
     if self.aggregate_comparison
-      weighted_current_score = results[:comparison_score] / (results[:matching_bills].size + results[:not_matching_bills].size)
-      weighted_aggregate_score = aggregate_comparison / bills_compared
-      self.aggregate_comparison = weighted_current_score + weighted_aggregate_score
+      current_compared = results[:matching_bills].size + results[:not_matching_bills].size
+      weighted_current_score = (results[:comparison_score] / 100) * current_compared
+      weighted_aggregate_score = (aggregate_comparison / 100) * bills_compared
+      weighted_total = weighted_current_score + weighted_aggregate_score
+      total_compared = current_compared + bills_compared
+      self.aggregate_comparison = (weighted_total / total_compared) * 100
     else
       self.aggregate_comparison = results[:comparison_score]
     end
