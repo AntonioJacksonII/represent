2e440edfe6028c340dee3b0c2586ace95611d1b5
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-09-27 10:00:58 -0600

    added caching action on page

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -1,7 +1,8 @@
 class RepresentativesController < ApplicationController
+  caches_action :index
   def index
     @members = HouseMember.by_state(params[:state_abbreviation])
     @senators = Senator.by_state(params[:state_abbreviation])
     @state = ::States[params[:state_abbreviation]]
-  end 
+  end
 end
